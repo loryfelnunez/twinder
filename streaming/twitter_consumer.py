47bcfe8f8af0893b6c9adac1c1a6503441347992
Loryfel Nunez <loryfelnunez@Lorys-MacBook-Pro.local>
2016-02-16 14:06:38 -0500

    clean up streaming code

--- streaming/twitter_consumer.py
+++ streaming/twitter_consumer.py
@@ -11,22 +11,13 @@ import time_uuid
 # Utility Functions
 ########################################################################################################################
 """
-This function parses a dictionary to grab two items - ZIP code and timestamp.
-Timestamp is reduced to Day, Hour, Minute and Second only.
-Input  : Dictionary
-Output : List (int, user_name, user_message)
+Input  : Dictionary from Twitter Data
+Output : List (user_name, time, user_message)
 """
 def parse_dictionary_retrieve_date(m):
-    # get user
-    #time = re.search('(\d\d\d\d\d\d(\d\d)-(\d\d\d\d\d\d))', m['timestamp_ms']) # grab day, hour, minute and second from timestamp
-    # check if message contains the topics
+    # get all the English tweets 
     if  m['lang'] == 'en':
-        #return (m['user']['screen_name'], m['text'], int(time.group(2) + "" + time.group(3)))
-        #return (m['user']['screen_name'], m['id_str'], m['text'], m['user']['id_str'], m['user']['description'], m['timestamp_ms'])
-        #return (m['user']['screen_name'], m['id_str'], m['text'])
         time_count = int(m['timestamp_ms'])
-        if time_count < 0:
-            time_count = time_count * (-1)
         return (m['user']['screen_name'], time_count, m['text'])
     else:
         placeholder = ""
@@ -44,13 +35,11 @@ if __name__ == "__main__":
     kafka_ip = k_in 
     # configure spark instance
     conf = (SparkConf().setMaster("s_master")\
-            .setAppName("get_topics_in_stream")\
+            .setAppName("get_realtime_twitter")\
             .set("spark.executor.memory", "1g")\
             .set("spark.cores.max", "3"))
     sc = SparkContext(conf = conf)
 
-    # get broadcast variables from cassandra
-    count = sc.accumulator(0)
     # stream every 5 seconds
     ssc = StreamingContext(sc, 5)
 
@@ -66,8 +55,7 @@ if __name__ == "__main__":
       #"time": x[5]
       })
 
-    #mapped = extracted.map(lambda line: line[0][0], line[0][1], line[1])) # return a list
-    my_row.saveToCassandra("test", "realtime_match4" )# save RDD to cassandra
+    my_row.saveToCassandra("twinder, "realtime_tweets" )# save RDD to cassandra
     ssc.start() # start the process
     ssc.awaitTermination()
 
